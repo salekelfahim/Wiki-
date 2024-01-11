ce83be02ac84f458f0360ba130779b091328808f
salekelfahim <salekelfahim@gmail.com>
2024-01-11 17:38:06 +0100

    Adding login page and working on login function

--- public/index.php
+++ public/index.php
@@ -10,12 +10,13 @@ use App\core\Application;
 require_once '../app/core/Application.php';
 require_once '../app/core/Router.php';
 require_once '../app/core/Request.php';
-// use app\core\Application;
-// use app\controllers\HomeController;
+
+
 $app = new Application();
 Router::get('/', 'home');
 Router::get('/registre', 'signup');
 Router::post('/registre',[UserController::class, 'signup']);
 Router::get('/login', 'login');
-// Router::post('/login',[HomeController::class, 'login']);
+Router::post('/login',[UserController::class, 'login']);
+
 $app->run(); 
