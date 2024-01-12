f9226f37c751a371873fe0133b5049a910bca360
salekelfahim <salekelfahim@gmail.com>
2024-01-12 16:24:27 +0100

    Adding AddWiki function

--- app/models/Wiki.php
+++ app/models/Wiki.php
@@ -4,10 +4,13 @@ require_once '../app/Database/Database.php';
 class Wiki
 {
 
-  private $nom;
-  private $description;
-  private $etat;
-  private $date;
+    private $id;
+    private $titre;
+    private $auteur;
+    private $categorie;
+    private $date;
+    private $status;
+    private $contenu;
 
 
   public function getAll()
@@ -17,13 +20,29 @@ class Wiki
     if ($result) {
       return $result;
     }
-      return $result;
+    return $result;
   }
-  public function getWiki($id){
+  public function getWiki($id)
+  {
     $sql = "SELECT * FROM  `wiki` WHERE id = $id";
     $result =  Database::connexion()->getPdo()->query($sql)->fetchAll(PDO::FETCH_OBJ);
     if ($result) {
-        return $result;
+      return $result;
     }
-}
+  }
+  public function addWiki()
+  {
+    Database::connexion()->getPdo()->beginTransaction();
+    $sql = "INSERT INTO `wiki`(`title`, `content`, `date`, `id_utilisateur`, `id_category`) 
+              VALUES (?,?,?,?,?)";
+    $stmt = Database::connexion()->getPdo()->prepare($sql);
+    $stmt->execute([$this->titre, $this->contenu, $this->date, $this->auteur, $this->categorie]);
+    if ($stmt) {
+      $last_id = Database::connexion()->getPdo()->query('SELECT MAX(id) FROM wiki')->fetchcolumn();
+      return $last_id;
+    } else {
+
+      return false;
+    }
+  }
 }
