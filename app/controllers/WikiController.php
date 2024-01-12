6dc22542c064f3f06f52443c4264e47df06e97ec
salekelfahim <salekelfahim@gmail.com>
2024-01-12 16:35:37 +0100

    Adding update and delete functions

--- app/controllers/WikiController.php
+++ app/controllers/WikiController.php
@@ -30,5 +30,65 @@ class WikiController
         return $this->router->renderView('details',["wiki"=>$wiki]);
     }
 
+    public function getAllCategories(){
+        $category = new Categorie();
+        return $category->getAllcategories();
+    }
     
+    public function getAllTags(){
+        $tag = new Tag();
+        return $tag->getAllTags();
+    }
+    
+    public function getAllCategoriesTags(){
+        $categories = $this->getAllCategories();
+        $tags = $this->getAllTags();
+        // var_dump($categories);die();
+        return $this->router->renderView('addwiki',  ["categories" => $categories, "tags" =>$tags]);
+    }
+
+
+    public function getMyWikis(){
+        $auteur = $_SESSION['id'];
+        $wikis = $this->wiki->getMywikis($auteur);
+        
+            return $this->router->renderView('mywiki',["wikis"=>$wikis]);
+
+        
+    }
+    
+
+    public function addWiki(){ 
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            if(!empty($_POST['title']) && !empty($_POST['content']) && !empty($_POST['tag']) && !empty($_POST['categotie']))
+            {
+                $title = $_POST['title'];
+                $content = $_POST['content'];
+                $tags = $_POST['tag'];
+                $categorie = $_POST['categotie'];
+                $auteur = $_SESSION['id'];
+                
+                $this->wiki->setAuteur($auteur);
+                $this->wiki->setTitre($title);
+                $this->wiki->setCategorie($categorie);
+                $this->wiki->setContenu($content);
+                $id=$this->wiki->addWiki();
+                foreach($tags as $tag){
+                    $tagWiki = new TagWiki;
+                    $tagWiki->setTag($tag);
+                    $tagWiki->setWiki($id);
+                    $tagWiki->addwikitag();
+                }
+                header('Location: /');
+            }
+        }
+
+    }
+    public function deletewiki(){
+        $id = $_GET['id'];
+        $this->wiki->deleteWiki($id);
+        header('Location: /MyWikis');
+    }
 }
+
+    
--- app/models/Wiki.php
+++ app/models/Wiki.php
@@ -4,15 +4,70 @@ require_once '../app/Database/Database.php';
 class Wiki
 {
 
-    private $id;
-    private $titre;
-    private $auteur;
-    private $categorie;
-    private $date;
-    private $status;
-    private $contenu;
+  private $id;
+  private $titre;
+  private $auteur;
+  private $categorie;
+  private $status;
+  private $contenu;
 
 
+  public function getId()
+  {
+    return $this->id;
+  }
+  public function setId($id)
+  {
+    $this->id = $id;
+  }
+
+  public function getTitre()
+  {
+    return $this->titre;
+  }
+  public function setTitre($titre)
+  {
+    $this->titre = $titre;
+  }
+
+  public function getAuteur()
+  {
+    return $this->auteur;
+  }
+
+  public function setAuteur($auteur)
+  {
+    $this->auteur = $auteur;
+  }
+
+  public function getCategorie()
+  {
+    return $this->categorie;
+  }
+
+  public function setCategorie($categorie)
+  {
+    $this->categorie = $categorie;
+  }
+
+
+  public function getStatus()
+  {
+    return $this->status;
+  }
+  public function setStatus($status)
+  {
+    $this->status = $status;
+  }
+
+  public function getContenu()
+  {
+    return $this->contenu;
+  }
+  public function setContenu($contenu)
+  {
+    $this->contenu = $contenu;
+  }
   public function getAll()
   {
     $sql = "SELECT * FROM `wiki`";
@@ -36,13 +91,32 @@ class Wiki
     $sql = "INSERT INTO `wiki`(`title`, `content`, `date`, `id_utilisateur`, `id_category`) 
               VALUES (?,?,?,?,?)";
     $stmt = Database::connexion()->getPdo()->prepare($sql);
-    $stmt->execute([$this->titre, $this->contenu, $this->date, $this->auteur, $this->categorie]);
+    $stmt->execute([$this->titre, $this->contenu, $this->auteur, $this->categorie]);
     if ($stmt) {
       $last_id = Database::connexion()->getPdo()->query('SELECT MAX(id) FROM wiki')->fetchcolumn();
       return $last_id;
     } else {
 
       return false;
+    }
+  }
+
+
+  public function getMywikis($auteur)
+  {
+    $sql = "SELECT * FROM  `wiki` WHERE id_utilisateur = $auteur";
+    $row = Database::connexion()->getPdo()->query($sql)->fetchAll(PDO::FETCH_OBJ);
+    if ($row) {
+      return $row;
+    }
+  }
+
+  public function deleteWiki($id)
+  {
+    $sql = "DELETE FROM `wiki` WHERE id = $id";
+    $result = Database::connexion()->getPdo()->query($sql);
+    if ($result) {
+      return true;
     }
   }
 }
